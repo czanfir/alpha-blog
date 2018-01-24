d3538a335e3b5667cf820803540b5f281188794f
Cristi Zanfir <zanfircristian92@gmail.com>
2018-01-24 17:43:58 +0000

    Add admin functionality for categories. Change navbar

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
   
   def setup
     @category = Category.create(name: "sports")
+    @admin_user = User.create(username: "test", email: "test@test.com", password:"test", admin: true)
   end
   
   test "should get categories index" do
@@ -12,6 +13,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get new" do
+    session[:user_id] = @admin_user.id #simulate that we have logged in as created admin_user
     get :new
     assert_response :success
   end
@@ -19,5 +21,12 @@ class CategoriesControllerTest < ActionController::TestCase
   test "should get show" do
     get(:show, {id: @category.id})
     assert_response :success
+  end
+  
+  test "should redirect when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: "sports" }
+    end
+    assert_redirected_to categories_path
   end
 end
